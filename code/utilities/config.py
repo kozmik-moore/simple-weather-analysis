9e5afb79814467204531da08ac71c0080f68bc52
Kozmik <koz.moore@gmail.com>
2025-07-01 18:11:54 -0700

    Processed weather values using a log-modulus transformation.

--- code/utilities/config.py
+++ code/utilities/config.py
@@ -11,6 +11,7 @@ products_dir = Path(join(root_dir, 'products'))
 # File paths
 raw_data_path = join(data_dir, 'raw_weather.csv')
 processed_data_path = join(data_dir, 'processed_weather.csv')
+processed_data_log_path = join(data_dir, 'processed_weather_log.csv')
 
 # Other variables
 weather_vars = [        # weather variables (as opposed to "time" or "location")
